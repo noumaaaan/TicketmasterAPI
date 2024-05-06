39196ad510097fd51ea51b4e37832afba0efe574
Nouman Mehmood <nouman_7@live.co.uk>
2024-05-06 02:51:38 +0100

    Initial Commit

---
+++ Ticketmaster/TicketmasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketmasterApp.swift
+//  Ticketmaster
+//
+//  Created by Noumaan Mehmood on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicketmasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
