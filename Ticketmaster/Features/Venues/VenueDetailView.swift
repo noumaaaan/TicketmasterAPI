751600c26f41853cf9cf100c37c7d74049f00213
Nouman Mehmood <nouman_7@live.co.uk>
2024-05-08 22:45:23 +0100

    Detail view map kit integration

--- Ticketmaster/Features/Venues/VenueDetailView.swift
+++ Ticketmaster/Features/Venues/VenueDetailView.swift
@@ -6,12 +6,105 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct VenueDetailView: View {
     let venue: TMVenue
+    
     var body: some View {
-        
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+//                TMImageView(
+//                    url: venue.images?.first?.url,
+//                    width: 300,
+//                    height: 170,
+//                    backgroundColor: .black,
+//                    cornerRadius: 4,
+//                    aspectRatio: .fill
+//                )
+//                .padding(.top)
+                
+                mapSection
+                    .padding(.top)
+                
+                VStack(alignment: .leading) {
+                    detailsSection
+                        .padding(.bottom)
+                    boxOfficeSection
+                        .padding(.bottom)
+                    parkingSeatingSection
+                        .padding(.bottom)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .navigationTitle(venue.name)
+    }
+}
+
+extension VenueDetailView {
+    
+    var detailsSection: some View {
+        VStack(alignment: .leading) {
+            Text("Details")
+                .font(.title3.bold())
+            Group {
+                Text(venue.name)
+                Text(venue.timezone)
+                Text(venue.address.line1)
+                if let line2 = venue.address.line2 { Text(line2) }
+                Text(venue.city.name)
+                Text(venue.postalCode)
+                Text(venue.country.name)
+            }
+            .font(.caption)
+        }
+    }
+    
+    var boxOfficeSection: some View {
+        VStack(alignment: .leading) {
+            if let boxOffice = venue.boxOfficeInfo {
+                Text("Box Office")
+                    .font(.title3.bold())
+                Group {
+                    if let phone = boxOffice.phoneNumberDetail { Text(phone) }
+                    if let hours = boxOffice.openHoursDetail { Text(hours) }
+                    if let payment = boxOffice.acceptedPaymentDetail { Text(payment) }
+                    if let call = boxOffice.willCallDetail { Text(call) }
+                }
+                .font(.caption)
+            }
+        }
+    }
+    
+    var parkingSeatingSection: some View {
+        VStack(alignment: .leading) {
+            Text("More information")
+                .font(.title3.bold())
+            Group {
+                if let parking = venue.parkingDetail { Text(parking) }
+                if let seating = venue.accessibleSeatingDetail { Text(seating) }
+            }
+            .font(.caption)
+        }
+    }
+    
+    var mapSection: some View {
+        VStack(alignment: .leading) {
+            Text("Map")
+                .font(.title3.bold())
+                .padding(.horizontal)
+            if let lat = Double(venue.location.latitude), let long = Double(venue.location.longitude) {
+                Map(bounds: .init(minimumDistance: 350, maximumDistance: 1000)) {
+                    Marker(venue.name, coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long))
+                }
+                .frame(height: 200)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
