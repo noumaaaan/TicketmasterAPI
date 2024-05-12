7339214b97474b3881a2101982a4e2d1b0bbcbbc
Nouman Mehmood <nouman_7@live.co.uk>
2024-05-12 07:41:28 +0100

    Encapsulate sections into groups for events details view

--- Ticketmaster/Views/Events /EventDetailView.swift
+++ Ticketmaster/Views/Events /EventDetailView.swift
@@ -26,8 +26,6 @@ struct EventDetailView: View {
                 seatInformation
                 limitInformation
                 classInformation
-                
-                
             }
             .padding()
         }
@@ -53,82 +51,95 @@ extension EventDetailView {
     }
     
     var salesInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let publicSales = event.sales?.publicSales {
-                Text("Sales")
-                    .font(.title2.bold())
-                if let start = publicSales.startDateTime { Text("Start: \(formattedDate(date: start))") }
-                if let end = publicSales.endDateTime { Text("End: \(formattedDate(date: end))") }
+                VStack(alignment: .leading) {
+                    Text("Sales")
+                        .font(.title2.bold())
+                    if let start = publicSales.startDateTime { Text("Start: \(formattedDate(date: start))") }
+                    if let end = publicSales.endDateTime { Text("End: \(formattedDate(date: end))") }
+                }
             }
         }
     }
     
     var datesInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let dates = event.dates {
-                Text("Dates")
-                    .font(.title2.bold())
-                if let start = dates.start?.dateTime { Text(formattedDate(date: start)) }
-                if let timezone = dates.timezone { Text(timezone) }
-                if let code = dates.status?.code { Text(code) }
+                VStack(alignment: .leading) {
+                    Text("Dates")
+                        .font(.title2.bold())
+                    if let start = dates.start?.dateTime { Text(formattedDate(date: start)) }
+                    if let timezone = dates.timezone { Text(timezone) }
+                    if let code = dates.status?.code { Text(code) }
+                }
             }
         }
     }
     
     var classInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let classification = event.classifications?.first {
-                Text("Classification")
-                    .font(.title2.bold())
-                
-                HStack {
-                    if let segment = classification.segment { RoundedTextView(text: segment.name )}
-                    if let genre = classification.genre { RoundedTextView(text: genre.name )}
-                    if let subGenre = classification.subGenre { RoundedTextView(text: subGenre.name )}
+                VStack(alignment: .leading) {
+                    Text("Classification")
+                        .font(.title2.bold())
+                    
+                    HStack {
+                        if let segment = classification.segment { RoundedTextView(text: segment.name )}
+                        if let genre = classification.genre { RoundedTextView(text: genre.name )}
+                        if let subGenre = classification.subGenre { RoundedTextView(text: subGenre.name )}
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
                 }
-                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.bottom)
             }
         }
     }
     
     var promoterInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let prom = event.promoter {
-                Text("Promoter")
-                    .font(.title2.bold())
-                if let promoter = prom.name { Text(promoter) }
-                if let description = prom.description { Text(description) }
+                VStack(alignment: .leading) {
+                    Text("Promoter")
+                        .font(.title2.bold())
+                    if let promoter = prom.name { Text(promoter) }
+                    if let description = prom.description { Text(description) }
+                }
             }
         }
     }
     
     var priceInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let price = event.priceRanges?.first {
-                Text("Prices from")
-                    .font(.title2.bold())
-                if let min = price.min { Text( "\(String(min)) \(price.currency ?? "")") }
-                if let max = price.max { Text( "\(String(max)) \(price.currency ?? "")") }
+                VStack(alignment: .leading) {
+                    Text("Prices from")
+                        .font(.title2.bold())
+                    if let min = price.min { Text( "\(String(min)) \(price.currency ?? "")") }
+                    if let max = price.max { Text( "\(String(max)) \(price.currency ?? "")") }
+                }
             }
         }
     }
     
     var productsInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let products = event.products {
-                Text("Products")
-                    .font(.title2.bold())
+                VStack(alignment: .leading) {
+                    Text("Products")
+                        .font(.title2.bold())
 
-                ForEach(products, id: \.self) { product in
-                    VStack(alignment: .leading) {
-                        if let name = product.name { Text(name) }
-                        if let url = product.url, let actual = URL(string: url) {
-                            Button {
-                                UIApplication.shared.open(actual)
-                            } label: {
-                                Text(url)
-                                    .truncationMode(.tail)
-                                    .lineLimit(1)
+                    ForEach(products, id: \.self) { product in
+                        VStack(alignment: .leading) {
+                            if let name = product.name { Text(name) }
+                            if let url = product.url, let actual = URL(string: url) {
+                                Button {
+                                    UIApplication.shared.open(actual)
+                                } label: {
+                                    Text(url)
+                                        .truncationMode(.tail)
+                                        .lineLimit(1)
+                                }
                             }
                         }
                     }
@@ -138,17 +149,19 @@ extension EventDetailView {
     }
     
     var seatInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let url = event.seatmap?.staticUrl, let actual = URL(string: url) {
                 VStack(alignment: .leading) {
-                    Text("Seatmap")
-                        .font(.title2.bold())
-                    Button {
-                        UIApplication.shared.open(actual)
-                    } label: {
-                        Text(url)
-                            .truncationMode(.tail)
-                            .lineLimit(1)
+                    VStack(alignment: .leading) {
+                        Text("Seatmap")
+                            .font(.title2.bold())
+                        Button {
+                            UIApplication.shared.open(actual)
+                        } label: {
+                            Text(url)
+                                .truncationMode(.tail)
+                                .lineLimit(1)
+                        }
                     }
                 }
             }
@@ -156,25 +169,29 @@ extension EventDetailView {
     }
     
     var limitInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let limit = event.ticketLimit?.info {
-                Text("Limit")
-                    .font(.title2.bold())
-                Text(limit)
+                VStack(alignment: .leading) {
+                    Text("Limit")
+                        .font(.title2.bold())
+                    Text(limit)
+                }
             }
         }
     }
     
     var venueInformation: some View {
-        VStack(alignment: .leading) {
+        Group {
             if let venue = event.embedded?.venues?.first {
-                Text("Venue")
-                    .font(.title2.bold())
-                
-                NavigationLink {
-                    VenueDetailView(venue: venue)
-                } label: {
-                    VenueView(venue: venue)
+                VStack(alignment: .leading) {
+                    Text("Venue")
+                        .font(.title2.bold())
+                    
+                    NavigationLink {
+                        VenueDetailView(venue: venue)
+                    } label: {
+                        VenueView(venue: venue)
+                    }
                 }
             }
         }
